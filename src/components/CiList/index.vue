713193b174d854ba5dfb3b5add92912bc002b28b
D1sappear <2066972867@qq.com>
2020-01-02 10:32:39 +0800

    miaomiao-cinema

---
+++ src/components/CiList/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    $END$
+  </div>
+</template>
+
+<script>
+  export default {
+    name: ''
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Welcome to Movie
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Movie'
+  }
+</script>
+
+<style scoped>
+
+</style>
